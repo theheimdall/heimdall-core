d7ee4cdf2343a1ffc99c9bbf05eb6a20ccc1758f
Lakshay Ghai <31838382+lakshayghai@users.noreply.github.com>
2022-08-07 18:33:24 -0400

    Initial Configuration (#1)
    * Initial Configuration
    
    - Added Spring Cloud gateway and webflux deps
    - Added Actuator dep
    - For Code formatting added Spotify fmt maven plugin

---
+++ src/main/java/org/theheimdall/heimdallcore/HeimdallCoreApplication.java
@@ -0,0 +1,12 @@
+package org.theheimdall.heimdallcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeimdallCoreApplication.class, args);
+  }
+}
---
+++ src/test/java/org/theheimdall/heimdallcore/HeimdallCoreApplicationTests.java
@@ -0,0 +1,11 @@
+package org.theheimdall.heimdallcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimdallCoreApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
